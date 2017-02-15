7990919677f489255b87a2cf881649479dc89597
pigats <andreapigato@gmail.com>
2017-02-15 23:25:46 +0100

    add admin models
    - device type
    - control
    - control type

---
+++ app/models/control.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    name: DS.attr('string'),
+    type: DS.belongsTo('controlType'),
+    values: DS.attr({ defaultValue: null })
+});
