e68c5e5754939480dff7d2b8de7c9c2268af9678
pigats <andreapigato@gmail.com>
2017-02-16 11:43:44 +0100

    add edit feature for select controls' options

--- app/components/add-control.js
+++ app/components/add-control.js
@@ -2,9 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     store: Ember.inject.service('store'),
+    selectOption: Ember.computed('selectOptions', function() {
+        let selectOptions = this.get('selectOptions');
+        return selectOptions[selectOptions.length - 1];
+    }),
 
     init() {
         this._super();
         this.set('controlTypes', this.get('store').findAll('controlType'));
+        this.set('selectOptions', this.get('control.control.values'));
+    },
+
+    actions: {
+        addSelectOption() {
+            this.get('selectOptions').pushObject({
+                value: ''
+            }); 
+        },
+
+        saveSelectOption(params) {
+            this.get('control.control.values').pushObject({
+                value: params
+            });
+        }
+
     }
 });
--- app/models/control.js
+++ app/models/control.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string'),
     type: DS.belongsTo('controlType'),
-    values: DS.attr({ defaultValue: null })
+    values: DS.attr({ defaultValue: () => [{value: ''}] })
 });
